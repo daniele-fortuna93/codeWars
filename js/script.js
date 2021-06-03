4f14c526612c945ba95f9c6ef59f7aaa8b9f5a61
Daniele <bambudio@libero.it>
2021-06-03 10:24:19 +0200

    Kata 6

--- js/script.js
+++ js/script.js
@@ -58,11 +58,29 @@
 // }
 
 
-Write a function to split a string and convert it into an array of words. For example:
+// Write a function to split a string and convert it into an array of words. For example:
 
-function stringToArray(string){
-  return string.split(' ');
+// function stringToArray(string){
+//   return string.split(' ');
+// }
+
+// console.log(stringToArray("I love arrays they are my favorite"));
+
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+You can assume all values in the array are numbers.
+
+
+function smallEnough(a, limit){
+  let result = true;
+  a.forEach((item) => {
+    if ( item > limit) {
+      return result = false;
+    }
+  });
+  return result;
 }
 
-console.log(stringToArray("I love arrays they are my favorite"));
+x = smallEnough([100,200,300,400], 500);
+console.log(x);
 
