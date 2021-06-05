2494334f10ef6493927044b7d944f4394603bee9
Daniele <bambudio@libero.it>
2021-06-05 14:05:07 +0200

    Kata 19

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 // Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
-
+ 
 // Example:
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
@@ -270,16 +270,29 @@
 //   }
 // }
 
-This Kata is intended as a small challenge for my students
-All Star Code Challenge #1
-Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG
-function NBAplayer(name, team, ppg){
-    this.name=name;
-    this.team=team;
-    this.ppg=ppg;
-  } 
-  var iverson = new NBAplayer("Iverson", "76ers", 11.2);
-  var jordan = new NBAplayer("Jordan", "bulls", 20.2);
-function sumPPG(playerOne, playerTwo){
-    return playerOne.ppg + playerTwo.ppg
+// This Kata is intended as a small challenge for my students
+// All Star Code Challenge #1
+// Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG
+// function NBAplayer(name, team, ppg){
+//     this.name=name;
+//     this.team=team;
+//     this.ppg=ppg;
+//   } 
+//   var iverson = new NBAplayer("Iverson", "76ers", 11.2);
+//   var jordan = new NBAplayer("Jordan", "bulls", 20.2);
+// function sumPPG(playerOne, playerTwo){
+//     return playerOne.ppg + playerTwo.ppg
+// }
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+function solution(string) {
+    string = string.split('');
+    for (let i = 0; i < string.length; i++) {
+        if ( string[i] == string[i].toUpperCase()) {
+            string[i] = ' ' + string[i]; 
+        }
+    }
+    string = string.join('');
+ return string
 }
