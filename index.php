e05a87965ce6404aa8d633aa212840ed6de5b2a8
Daniele <bambudio@libero.it>
2021-06-25 19:38:16 +0200

    Kata 28

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
 
 
     // Build a square
-    function generateShape($int) {
-        $row = "";
+    // function generateShape($int) {
+    //     $row = "";
         
-        for ($i=0; $i < $int; $i++) { 
-        $row .= "+";
-        }
-        $string = $row;
-        for ($i=0; $i < $int - 1; $i++) { 
-        $string .= "/n" . $row;
-        }
-        return $string;
+    //     for ($i=0; $i < $int; $i++) { 
+    //     $row .= "+";
+    //     }
+    //     $string = $row;
+    //     for ($i=0; $i < $int - 1; $i++) { 
+    //     $string .= "/n" . $row;
+    //     }
+    //     return $string;
+    // }
+
+
+    // Remove exlamation marks
+    function remove_exclamation_marks($string) {
+        return str_replace('!', "", $string);
     }
 
     ?>
