ff37d2725bb376d845dd6277d444050974a16c44
Daniele <bambudio@libero.it>
2021-06-25 19:42:29 +0200

    Kata 32

--- index.php
+++ index.php
@@ -24,9 +24,23 @@
 
     // Get the mean og an array 
 
-    function get_average($a) {
-        $result = array_sum($a) / count($a);
-        return floor($result);
-    }
+    // function get_average($a) {
+    //     $result = array_sum($a) / count($a);
+    //     return floor($result);
+    // }
+
+    // Quarter of the year
+
+    function quarterOf($month) {
+    
+        if ( $month > 9) {
+          return 4;
+        } else if ( $month > 6) {
+          return 3;
+        } else if ( $month > 3) {
+          return 2;
+        } 
+        return 1;
+      }
 
     ?>
